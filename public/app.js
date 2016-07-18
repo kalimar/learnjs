4676baf1805227d64decd6ea58e384173d54d7af
Kalimar Maia <kalimar.maia@gmail.com>
2016-07-18 07:22:44 -0400

    Start building router

---
+++ public/app.js
@@ -0,0 +1,16 @@
+'use strict';
+var learnjs = {};
+
+learnjs.probemView = function() {
+  return $('<div class="problem-view">').text('Coming soon!');
+}
+
+learnjs.showView = function(hash) {
+  var routes = {
+    '#problem-1': learnjs.problemView
+  };
+  var viewFn = routes[hash];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn());
+  }
+}
